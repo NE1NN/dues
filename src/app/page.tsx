1143c891f4f94fc457edf1a67e66121fec5c520e
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-10-30 15:21:19 +1100

    Add assignment to div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../firebase/service';
 
+type Course = {
+  id: string;
+  assignment: string;
+};
+
 export async function getCourses() {
   const coursesCollection = collection(db, 'courses');
   const courseDocs = await getDocs(coursesCollection);
 
-  const courses = courseDocs.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  }));
+  const courses: Course[] = courseDocs.docs.map(
+    (doc) =>
+      ({
+        id: doc.id,
+        ...doc.data(),
+      } as Course)
+  );
 
   return courses;
 }
 
 export default async function Home() {
   const courses = await getCourses();
-  console.log(courses);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Helloy
+    <main className="flex min-h-screen items-center justify-between p-24">
+      {courses.map((course) => (
+        <div key={course.id}>{course.assignment}</div>
+      ))}
     </main>
   );
 }
