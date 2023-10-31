a387a0e975142ca3bf1375760da6c7d6eaa3cf85
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2023-10-31 22:53:15 +1100

    ok

---
+++ firebase/data.ts
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: 1,
+    courseCode: "COMP1511",
+    assessmentItems: [
+      {
+        assName: "Problem sets",
+        weight: 15,
+        startWeek: 2,
+        endWeek: 10,
+        dueDate: "17/11/2023",
+        status: "ongoing",
+      },
+      {
+        assName: "Assignment 1",
+        weight: 20,
+        startWeek: 4,
+        endWeek: 7,
+        dueDate: "17/10/2023",
+        status: "ongoing",
+      },
+      {
+        assName: "Assignment 2",
+        weight: 25,
+        startWeek: 7,
+        endWeek: 10,
+        dueDate: "17/11/2023",
+        status: "not started",
+      },
+      {
+        assName: "Final Exam",
+        weight: 40,
+        startWeek: 1,
+        endWeek: 11,
+        dueDate: "30/10/2023",
+        status: "not started",
+      }
+    ],
+    progress: 30,
+    totalCourseMark: 25
+  }
+]
