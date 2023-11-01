37f6a66d0ebb594dac7213c125a8cbd9fb793b56
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-02 03:58:33 +1100

    Remove commented lines

--- src/app/components/DueAssessments.tsx
+++ src/app/components/DueAssessments.tsx
@@ -1,25 +1,11 @@
-// export default function DueAssessments({ assessments }: AssContainerProps) {
-//   return (
-//     <div>
-//       <h1 className="font-bold text-black mt-4 text-2xl">Due assessments</h1>
-//       {assessments.map((assessment, idx) => (
-//         <DueList
-//           key={`${assessment.id}${idx}`}
-//           assessment={assessment}
-//         ></DueList>
-//       ))}
-//     </div>
-//   );
-// }
-
-import { AssContainerProps } from "../../../types/types";
-import DueList from "./DueList";
-import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { AssContainerProps } from '../../../types/types';
+import DueList from './DueList';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function DueAssessments({ assessments }: AssContainerProps) {
   return (
@@ -29,23 +15,28 @@ export default function DueAssessments({ assessments }: AssContainerProps) {
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
-          style={{ position: 'sticky', top: 0, zIndex: 1, backgroundColor: 'white' }}
+          style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+            backgroundColor: 'white',
+          }}
         >
           <Typography className="font-bold text-black mt-4 text-2xl">
             Due Assessments
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-            {assessments.length > 0 ? (
-              assessments.map((assessment, idx) => (
-                <DueList
-                  key={`${assessment.id}${idx}`}
-                  assessment={assessment}
-                ></DueList>
-              ))
-            ) : (
-              <Typography>No assessments due.</Typography>
-            )}
+          {assessments.length > 0 ? (
+            assessments.map((assessment, idx) => (
+              <DueList
+                key={`${assessment.id}${idx}`}
+                assessment={assessment}
+              ></DueList>
+            ))
+          ) : (
+            <Typography>No assessments due.</Typography>
+          )}
         </AccordionDetails>
       </Accordion>
     </div>
