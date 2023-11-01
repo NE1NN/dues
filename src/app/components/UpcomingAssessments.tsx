5b2be5d170f6e22bfb33ab7be3b39660e74caaef
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-02 03:49:31 +1100

    Change upcoming deadlines position

--- src/app/components/UpcomingAssessments.tsx
+++ src/app/components/UpcomingAssessments.tsx
@@ -1,11 +1,11 @@
-import { AssContainerProps } from "../../../types/types";
-import DueList from "./DueList";
-import * as React from "react";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { AssContainerProps } from '../../../types/types';
+import DueList from './DueList';
+import * as React from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function UpcomingAssessments({
   assessments,
@@ -17,23 +17,28 @@ export default function UpcomingAssessments({
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
           id="panel1a-header"
-          style={{ position: 'sticky', top: 0, zIndex: 1, backgroundColor: 'white' }}
+          style={{
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+            backgroundColor: 'white',
+          }}
         >
           <Typography className="font-bold text-black mt-4 text-2xl">
             Upcoming Deadlines
           </Typography>
         </AccordionSummary>
         <AccordionDetails>
-            {assessments.length > 0 ? (
-              assessments.map((assessment, idx) => (
-                <DueList
-                  key={`${assessment.id}${idx}`}
-                  assessment={assessment}
-                ></DueList>
-              ))
-            ) : (
-              <Typography>No upcoming deadlines.</Typography>
-            )}
+          {assessments.length > 0 ? (
+            assessments.map((assessment, idx) => (
+              <DueList
+                key={`${assessment.id}${idx}`}
+                assessment={assessment}
+              ></DueList>
+            ))
+          ) : (
+            <Typography>No upcoming deadlines.</Typography>
+          )}
         </AccordionDetails>
       </Accordion>
     </div>
