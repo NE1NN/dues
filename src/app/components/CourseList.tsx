8d94b429688295f3e9e9552e9928ed5f3138f4b8
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-02 04:21:56 +1100

    Change color when clicked

--- src/app/components/CourseList.tsx
+++ src/app/components/CourseList.tsx
@@ -22,7 +22,7 @@ export default function CourseList({ course }: CourseListProps) {
     <div
       className={`w-full text-lg ${
         clickedCourse === course.courseCode
-          ? `bg-green-700 text-white`
+          ? `bg-green-1000 text-white`
           : `bg-green-600`
       } text-center flex-1 text-black flex flex-col justify-center items-center cursor-pointer hover:bg-opacity-80 hover:bg-green-1000 hover:text-white transition ease-in-out rounded-lg`}
       onClick={(e) => handleClick(e)}
