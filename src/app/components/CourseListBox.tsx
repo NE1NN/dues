18ef1138b4522bbcaa434c9a3a871e4fddc0ebdb
Hans Kristian Reynaldi <hanskr7882@gmail.com>
2023-11-02 00:02:02 +1100

    yah

--- src/app/components/CourseListBox.tsx
+++ src/app/components/CourseListBox.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { MouseEvent, useContext } from 'react';
-import SelectedCoursesContext from './SelectedCoursesContext';
-import CourseList from './CourseList';
-import { CourseListBoxProps } from '../../../types/types';
-import { LockOpen } from '@mui/icons-material';
-import LockIcon from '@mui/icons-material/Lock';
+import { MouseEvent, useContext } from "react";
+import SelectedCoursesContext from "./SelectedCoursesContext";
+import CourseList from "./CourseList";
+import { CourseListBoxProps } from "../../../types/types";
+import { LockOpen } from "@mui/icons-material";
+import LockIcon from "@mui/icons-material/Lock";
 
 export default function CourseListBox({
   isLocked,
@@ -15,7 +15,7 @@ export default function CourseListBox({
 
   if (!contextValue) {
     throw new Error(
-      'SelectedCoursesContext must be used within a SelectedCoursesContext.Provider'
+      "SelectedCoursesContext must be used within a SelectedCoursesContext.Provider"
     );
   }
 
@@ -29,10 +29,14 @@ export default function CourseListBox({
   return (
     <aside className="w-[30%] bg-green-500 h-full ml-auto flex flex-col p-8 justify-between gap-8 relative">
       <div
-        className="absolute right-0 top-0 cursor-pointer"
+        className="absolute right-0 top-0 cursor-pointer bg-black rounded-full w-9 h-9 flex items-center justify-center"
         onClick={(e) => handleLockClick(e)}
       >
-        {isLocked ? <LockIcon></LockIcon> : <LockOpen></LockOpen>}
+        {isLocked ? (
+          <LockIcon style={{ color: "#a3b18a" }}></LockIcon>
+        ) : (
+          <LockOpen></LockOpen>
+        )}
       </div>
       {selectedCourses.map((course) => (
         <CourseList key={course.id} course={course}></CourseList>
