e007820a7a90f875e3f93a001ee816db029f8c18
Hindie Suputra <z5372377@ad.unsw.edu.au>
2023-11-03 17:34:05 +1100

    Change the position of login

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -1,7 +1,11 @@
-'use client'
+'use client';
 
 import { upgradeAnonymousToGoogle } from '../../../firebase/auth';
 
 export default function Login() {
-  return <button onClick={upgradeAnonymousToGoogle}>Login</button>;
+  return (
+    <button onClick={upgradeAnonymousToGoogle} className="ml-auto">
+      Login
+    </button>
+  );
 }
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Login from './Login';
 
 export default function Navbar() {
   return (
-    <nav className="bg-green-600 px-4 py-2 font-bold text-green-900 text-2xl">
+    <nav className="bg-green-600 px-4 py-2 font-bold text-green-900 text-2xl flex">
       Dues
       <Login></Login>
     </nav>
